af82fbbd77bc6aead75ad2bfb43693c2f7625684
sudam wanve <sudam.wanve@infobeans@infobeans.com>
2023-08-21 14:19:41 +0530

    Falsk templates basic,urlfor,inheritance examples

---
+++ flask_project/Templates_basic/controlflow.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    puppies = ['Fluffy','Rufus','Spike']
+    return render_template('controlflow.html',
+                           puppies=puppies)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
